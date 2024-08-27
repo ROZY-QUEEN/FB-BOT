85c403eb16d50166f72c5113f082e6400098a571
ROZY-QUEEN <rozythakur32@gmail.com>
2024-08-27 11:22:50 +0530

    Update 0_PREM-ANTI-OUT.js

--- modules/events/0_PREM-ANTI-OUT.js
+++ modules/events/0_PREM-ANTI-OUT.js
@@ -11,12 +11,12 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (data.antiout == false) return;
  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
- const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "मेरा बॉस खुद ही भाग गया 😄✌️";
+ const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "मेरा एडमिन खुद ही भाग गयी 😄✌️";
  if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
     api.sendMessage(`😏😏😏😏😏`, event.threadID)
-   } else api.sendMessage(`🥱🥱🥱🥱🥱`, event.threadID);
+   } else api.sendMessage(`🥱`, event.threadID);
   })
  }
 }
